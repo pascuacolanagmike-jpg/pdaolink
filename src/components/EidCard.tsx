c824916df8604062e880c1e1f52f99420e537cff
pascuacolanagmike-jpg <pascuacolanagmike@gmail.com>
2026-09-07 02:30:09 +0800

    Update EidCard.tsx

--- src/components/EidCard.tsx
+++ src/components/EidCard.tsx
@@ -180,7 +180,7 @@ export default function EidCard({ application, photoUrl, flipped, onFlip }: EidC
             <div
               style={{
                 position: 'absolute',
-                top: '68%',
+                top: '66%',
                 left: '5%',
                 width: '52%',
                 height: '5%',
